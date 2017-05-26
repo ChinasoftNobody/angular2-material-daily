ef75f6997e02e9c2f1a885f5c80f8114eb0b5a24
liguanghao <chiansoft_lgh@outlook.com>
2017-05-26 22:51:56 +0800

    angular2-material-daily

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {MdButtonModule, MdCheckboxModule, MdDatepickerModule, MdInputModule, MdSidenavModule} from "@angular/material";
+
+import { AppComponent } from './app.component';
+import 'hammerjs';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MdButtonModule,
+    MdSidenavModule,
+    MdInputModule,
+    MdDatepickerModule,
+    MdCheckboxModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
