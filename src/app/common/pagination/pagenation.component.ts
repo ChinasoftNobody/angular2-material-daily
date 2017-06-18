0edb2b5b06e3db2d261f4e593a20eaf6d42f83e3
liguanghao <chiansoft_lgh@outlook.com>
2017-06-18 23:18:55 +0800

    daily 分页插件

--- src/app/common/pagination/pagenation.component.ts
+++ src/app/common/pagination/pagenation.component.ts
@@ -15,8 +15,19 @@ export class PaginationComponent implements OnInit, OnChanges {
   @Output() onSelect: EventEmitter<any> = new EventEmitter<any>();
   _pages: number[] = [];
   _pageNumber: number;
+  _gridCols = 0;
+  _morePositionPre = true;
+  _morePositionShow = false;
 
   onSelectPage(pageNumber: number) {
+    if (pageNumber < 0) {
+      pageNumber ++;
+      return;
+    }
+    if (pageNumber > this._pages.length - 1) {
+      pageNumber --;
+      return;
+    }
     this.onSelect.emit({size: this.size, number: pageNumber});
   }
 
@@ -27,16 +38,35 @@ export class PaginationComponent implements OnInit, OnChanges {
     for (const propName in changes) {
       if (propName === 'total') {
         this.total = changes[propName].currentValue;
-      }else if (propName === 'size') {
+      } else if (propName === 'size') {
         this.size = changes[propName].currentValue;
-      }else if (propName === 'number') {
+      } else if (propName === 'number') {
         this.number = changes[propName].currentValue;
       }
     }
-    this._pageNumber = Math.ceil(this.total / this.size);
+    const number = Math.ceil(this.total / this.size);
+    if (number > 10) {
+      this._morePositionShow = true;
+      if (this.number > number / 2) {
+        this._morePositionPre = true;
+      } else {
+        this._morePositionPre = false;
+      }
+      this._pageNumber = 10;
+    } else {
+      this._morePositionShow = false;
+      this._pageNumber = number;
+    }
+    this._gridCols = this._pageNumber + 2 + (this._morePositionShow ? 1 : 0);
     this._pages = [];
-    for (let i = 0; i < this._pageNumber; i++) {
-      this._pages.push(i);
+    if (!this._morePositionShow) {
+      for (let i = 0; i < this._pageNumber; i++) {
+        this._pages.push(i);
+      }
+    } else {
+      for (let i = 0; i < this._pageNumber; i++) {
+        this._pages.push(i);
+      }
     }
   }
 }
