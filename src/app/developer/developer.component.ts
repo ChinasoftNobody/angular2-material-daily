e7abba3f7b61aba8fa60e8ffe3361ec5668a6d4f
liguanghao <chiansoft_lgh@outlook.com>
2017-07-22 20:37:18 +0800

    daily 日程

---
+++ src/app/developer/developer.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+/**
+ * Created by Administrator on 2017/7/22.
+ */
+@Component({
+  moduleId: module.id,
+  selector: 'app-developer',
+  templateUrl: './developer.component.html',
+  styleUrls: ['./developer.component.css'],
+  providers: []
+})
+export class DeveloperComponent {
+}
